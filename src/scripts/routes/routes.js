c84b99512ff5293d45e44fedcbf94c2a6f031df7
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-02-08 20:46:55 +0700

    feat: :art: remove unused datajson and add routes web app shell

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,9 @@
+import List from '../views/pages/list';
+// import Detail from '../views/pages/detail';
+
+const routes = {
+  '/': List, // default page
+  // '/detail/:id': Detail,
+};
+
+export default routes;
