29d2f6b3dfcfb337c872dd14c521d59701453871
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-02-14 00:46:42 +0700

    fix: :bug: update workbox strategies method

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-restricted-globals */
 import * as WorkboxWindow from 'workbox-window';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
+import { cleanupOutdatedCaches } from 'workbox-precaching';
 
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
@@ -8,8 +12,31 @@ const swRegister = async () => {
 
   const wb = new WorkboxWindow.Workbox('./service-worker.js');
 
+  wb.addEventListener('activated', () => {
+    // Get the current page URL + all resources the page loaded.
+    const urlsToCache = [
+      window.location.href,
+      ...performance.getEntriesByType('resource').map((r) => r.name),
+    ];
+    // Send that list of URLs to your router in the service worker.
+    wb.messageSW({
+      type: 'CACHE_URLS',
+      payload: { urlsToCache },
+    });
+
+    // register route to cache strategies only network first for review and detail path
+    registerRoute(
+      ({ url }) =>
+        url.pathname.startsWith('/review/') ||
+        url.pathname.startsWith('/detail/'),
+      new NetworkFirst(),
+    );
+  });
+
   try {
     await wb.register();
+    cleanupOutdatedCaches();
+
     // console.log('Service worker registered');
   } catch (error) {
     // console.log('Failed to register service worker', error);
