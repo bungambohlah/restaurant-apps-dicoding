d41985f89ea107a27ec4e745e361b72cffdec002
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-02-21 22:44:22 +0700

    fix: :zap: improve some web performance for styling and image asset

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,8 +1,8 @@
 export const BASE_URL = 'https://restaurant-api.dicoding.dev';
 export const BASE_IMAGE_URL =
-  'https://restaurant-api.dicoding.dev/images/medium';
+  'https://restaurant-api.dicoding.dev/images/small';
 export const BASE_LARGE_IMAGE_URL =
-  'https://restaurant-api.dicoding.dev/images/large';
+  'https://restaurant-api.dicoding.dev/images/medium';
 export const DATABASE_NAME = 'restaurant-database';
 export const DATABASE_VERSION = 1;
 export const OBJECT_STORE_NAME = 'restaurant';
