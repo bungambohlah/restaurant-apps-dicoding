b7af26b28082864e4e8aacf96b1e80c32886dd05
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-02-21 21:13:21 +0700

    fix: :green_heart: fix CI build to run e2e test in the staging mode

--- codecept.conf.js
+++ codecept.conf.js
@@ -12,7 +12,7 @@ exports.config = {
   output: 'e2e/outputs',
   helpers: {
     Puppeteer: {
-      url: 'http://127.0.0.1:9000',
+      url: 'https://restaurantappdicoding-staging.afif.dev',
       show: false,
       windowSize: '1200x900',
     },
