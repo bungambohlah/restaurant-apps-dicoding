6eb01951d57a4315219fed6e99aa3c9c60ab5a42
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-02-21 21:29:48 +0700

    test: :white_check_mark: add e2e test for check unliking the liked restaurant

--- e2e/outputs/Liking_Restaurants.spec.js
+++ e2e/outputs/Liking_Restaurants.spec.js
@@ -70,3 +70,86 @@ Scenario(
     }
   },
 );
+
+Scenario(
+  'liking one restaurant then unliking the liked restaurant',
+  async ({ I }) => {
+    I.amOnPage('/#/');
+
+    I.waitForElement('#restaurants-container restaurant-card', 5);
+
+    const firstRestaurant = locate(
+      '#restaurants-container restaurant-card',
+    ).first();
+    const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+
+    I.forceClick(firstRestaurant);
+
+    I.waitForElement('#likeButton', 5);
+    I.forceClick('#likeButton');
+
+    I.amOnPage('/#/favorite');
+    I.waitForElement('#restaurants-container restaurant-card', 3);
+    const likedRestaurants = locate(
+      '#restaurants-container restaurant-card',
+    ).first();
+    const likedRestaurantName = await I.grabTextFrom(likedRestaurants);
+
+    assert.strictEqual(firstRestaurantName, likedRestaurantName);
+
+    I.forceClick(likedRestaurants);
+
+    I.waitForElement('#likeButton', 5);
+    I.forceClick('#likeButton');
+
+    I.amOnPage('/#/favorite');
+
+    I.seeElement('#restaurants-container error-card[favoriteempty="true"]');
+  },
+);
+
+Scenario(
+  'liking three restaurant then unliking all liked restaurant',
+  async ({ I }) => {
+    I.amOnPage('/#/');
+
+    I.waitForElement('#restaurants-container restaurant-card', 5);
+
+    const names = [];
+
+    for (let i = 1; i <= 3; i++) {
+      const selectedRestaurant = locate(
+        '#restaurants-container restaurant-card',
+      ).at(i);
+      const selectedRestaurantName = await I.grabTextFrom(selectedRestaurant);
+      names.push(selectedRestaurantName);
+
+      I.forceClick(selectedRestaurant);
+
+      I.waitForElement('#likeButton', 5);
+      I.forceClick('#likeButton');
+
+      I.amOnPage('/#/');
+    }
+
+    I.amOnPage('/#/favorite');
+    I.waitForElement('#restaurants-container restaurant-card', 3);
+
+    for (let i = 1; i <= 3; i++) {
+      const likedRestaurants = locate(
+        '#restaurants-container restaurant-card',
+      ).at(i);
+      const likedRestaurantName = await I.grabTextFrom(likedRestaurants);
+      assert.strictEqual(names[i - 1], likedRestaurantName);
+
+      I.forceClick(likedRestaurants);
+      I.waitForElement('#likeButton', 5);
+      I.forceClick('#likeButton');
+
+      I.amOnPage('/#/');
+    }
+
+    I.amOnPage('/#/favorite');
+    I.seeElement('#restaurants-container error-card[favoriteempty="true"]');
+  },
+);
